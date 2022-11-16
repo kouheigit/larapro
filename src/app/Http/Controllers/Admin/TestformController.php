c11b3ad859a13aecfffeba62f89b3fed8b8d61d8
kouheigit <ookawakouhei777@gmail.com>
2022-11-16 13:48:44 +0900

    testform dev 3

--- src/app/Http/Controllers/Admin/TestformController.php
+++ src/app/Http/Controllers/Admin/TestformController.php
@@ -2,19 +2,22 @@
 
 namespace App\Http\Controllers\Admin;
 
+use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class TestformController extends Controller
 {
-    public function index(Request $request){
-        return view('admin.testform',compact('request'));
+    public function index(){
+        return view('admin.testform');
     }
     public function indexpost(Request $request){
         $name = $request->input('name');
         $comment = $request->input('comment');
         $samp = $request->input('samp');
         $samp1 = $request->input('samp1');
+        $value = $request->input('value');
 
+        dd($value);
         //insert method;
     }
 }
--- src/resources/views/admin/testform.blade.php
+++ src/resources/views/admin/testform.blade.php
@@ -1,19 +1,20 @@
 <h1>フォームテスト</h1>
-<form method="POST" action="{{('admin.testform')}}">
+<form method="POST" action="{{('testform')}}">
     @for($i=0; $i<3; $i++)
         @csrf
         <table>
-            <b>フォーム</b>
+            <b>フォーム{{$i}}</b>
+            <input type="hidden" name="value[]" value="{{$i}}">
             <ul>
-                <li>名前:<input type="text" name="name[]"value="{{old('name')}}"></li>
-                <br>
-                <li>コメント:<input type="text" name="comment[]"value="{{old('comment')}}"></li>
-                <br>
+                <li>名前:<br><input type="text" name="name[]"value="{{old('name[]')}}"></li>
+
+                <li>コメント:<br><input type="text" name="comment[]"value="{{old('comment[]')}}"></li>
+
                 <li>チェック<input type="checkbox" name="samp[{{$i}}]"value="{{$i}}"></li>
-                <br>
+
                 <li>チェック1<input type="checkbox" name="samp1[{{$i}}]"value="{{$i}}"></li>
-                <br>
-                <li>備考<input type="text" name="remark[]"value="{{old('remark')}}"></li>
+
+                <li>備考<br><input type="text" name="remark[]"value="{{old('remark[]')}}"></li>
             </ul>
         </table>
     @endfor
