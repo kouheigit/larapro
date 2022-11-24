adb606faf01be3b1ee5f03f8e43ecdbf9d93061c
kouheigit <ookawakouhei777@gmail.com>
2022-11-24 18:27:15 +0900

    testform dev24

--- src/resources/views/admin/someform.blade.php
+++ src/resources/views/admin/someform.blade.php
@@ -1,6 +1,35 @@
 <h1 id="choice">ここのテキストを変更する</h1>
 <script>
     /*
+    var number = Math.floor(Math.random() * 5);
+    var answer = parseInt(window.prompt("1~5までの数字を入力してください"));
+    if(answer == number) {
+        message = "あたり";
+    }else if(answer > 5) {
+        message = "0~5の数値を入力してください。"
+    }else if(answer > number){
+        message = "入力された答えの方が数値が大きいです。";
+    }else if(answer < number) {
+        message = "数字の方が大きいです";
+    }else{
+        message ="0~5の値を入力してください"
+    }
+    window.alert(message);
+
+
+    var answer = window.prompt('ゲームを開始しますか?')
+    if(answer=='yes') {
+        window.alert("ゲームを開始します");
+    }else if(answer=='no'){
+        window.alert('ゲームを終了します');
+    }else{
+        window.alert('yesかnoで入力してください');
+    }
+
+
+    var answer = window.prompt('ヘルプを見ますか?');
+    console.log(answer);
+
     if(window.confirm("ゲームスタート!準備はいい?")){
         window.alert("ゲームを開始します");
     }else{
