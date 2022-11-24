7eb49bb6b9467233ea4e7ce17c79397e3651ca8b
kouheigit <ookawakouhei777@gmail.com>
2022-11-24 19:18:48 +0900

    Writing objects: 100% (7/7), 541 bytes | 541.00 KiB/s, done. Total 7 (delta 6), reused 0 (delta 0), pack-reused 0 remote: Resolving deltas: 100% (6/6), completed with 6 local objects. To https://github.com/kouheigit/larapro.git    adb606f..cfd00a2  main -> main branch 'main' set up to track 'origin/main'. user@USERnoMacBook-Air larapro % git log commit cfd00a29040e7073efde2a0e6e3184cdae92efa8 (HEAD -> main, origin/main) Author: kouheigit <ookawakouhei777@gmail.com> Date:   Thu Nov 24 18:29:25 2022 +0900
         testform dev25
    
     testform dev26

--- src/resources/views/admin/someform.blade.php
+++ src/resources/views/admin/someform.blade.php
@@ -1,5 +1,37 @@
 <h1 id="choice">ここのテキストを変更する</h1>
+<h1 id="output">新たなテキスト</h1>
 <script>
+    /*
+    var todo = ['デザインカンプ作成','マズイ高い速い','宮本天外','ぐんぴぃ','Syamuさん'];
+    for(var i = 0; i<todo.length; i++){
+        console.log(todo[i]);
+    }
+
+   var fizzbuzz = function(num){
+       if(num % 3 == 0 || num % 5 == 0){
+            return 'fizzbuzz';
+       }else if(num % 3 == 0 ){
+           return'fizz';
+       }else if(num % 5 == 0){
+           return 'buzz';
+       }else{
+          return num;
+       }
+   }
+   for(var i=0; i<=10; i++) {
+       console.log(fizzbuzz(i));
+   }
+    /*
+    var total = function(price){
+        var tax = 0.08;
+        return price + price * tax;
+    }
+    console.log('コーヒーメーカーの値段は'+total(8000)+'円(税込です)');
+    document.getElementById('output').textContent ='コーヒーメーカの値段は'+total(8000)+'円です';
+
+    for(var i=0; i<=10; i++){
+        console.log(i);
+    }
 
     var number = Math.floor(Math.random() * 5);
     var answer = parseInt(window.prompt("1~5までの数字を入力してください"));
@@ -35,6 +67,6 @@
     }else{
         window.alert("ゲームを終了します");
     }
-
+*/
  document.getElementById('choice').textContent = new Date();
 </script>
