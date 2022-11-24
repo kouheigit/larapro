84570aa223421eded81f2f8be4e01c495bdc3b16
kouheigit <ookawakouhei777@gmail.com>
2022-11-24 17:30:19 +0900

    testform dev24

--- src/resources/views/admin/someform.blade.php
+++ src/resources/views/admin/someform.blade.php
@@ -1,2 +1,11 @@
-<h1>強くなりたいと願い抜いた</h1>
+<h1 id="choice">ここのテキストを変更する</h1>
+<script>
+    /*
+    if(window.confirm("ゲームスタート!準備はいい?")){
+        window.alert("ゲームを開始します");
+    }else{
+        window.alert("ゲームを終了します");
+    }*/
 
+ document.getElementById('choice').textContent = new Date();
+</script>
