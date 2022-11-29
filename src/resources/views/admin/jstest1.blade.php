b85a15fa7a36a355c0dd91cc5fdd3d00d8f0c336
kouheigit <ookawakouhei777@gmail.com>
2022-11-29 14:38:17 +0900

    testform dev37

--- src/resources/views/admin/jstest1.blade.php
+++ src/resources/views/admin/jstest1.blade.php
@@ -1,1 +1,27 @@
-<h1>坂道</h1>
+<form method="GET" action="{{('jstest2')}}">
+    @csrf
+    <table>
+        <b>映画館に行きますか?</b>
+        <br>
+        <input type="radio" name="frequency">週1回以上<br>
+        <input type="radio" name="frequency">月に1回くらい<br>
+        <input type="radio" name="frequency">1年に数回程度<br>
+        <input type="radio" name="frequency">ほとんど行かない、映画は観ない<br>
+    </table>
+    <br>
+    <input type="submit" value="送信する">
+</form>
+<script src="https://cdn.jsdelivr.net/npm/js-cookie@2/src/js.cookie.min.js"></script>
+<script>
+    
+    document.getElementById('form').onsubmit = function(){
+        if(Cookies.get('answered') === 'yes'){
+            window.alert('回答済みです、アンケートは1回しか入力できません');
+            return false;
+        }else{
+            Cookies.set('answered','yes', {expires: 7});
+        }
+    };
+</script>
+
+
--- src/resources/views/admin/jstest2.blade.php
+++ src/resources/views/admin/jstest2.blade.php
@@ -1,1 +1,14 @@
-<h1>送信後</h1>
+<section>
+    <p>ありがとうございます。</p>
+</section>
+<script src="https://cdn.jsdelivr.net/npm/js-cookie@2/src/js.cookie.min.js"></script>
+<script>
+    document.getElementById('form').onsubmit = function(){
+        if(Cookies.get('answered') === 'yes'){
+            window.alert('回答済みです、アンケートは1回しか入力できません');
+            return false;
+        }else{
+            Cookies.set('answerd','yes', {experies: 7});
+        }
+    };
+</script>
