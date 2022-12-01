efe7df04c386e958a01b8b45703c5b218c362eb4
kouheigit <ookawakouhei777@gmail.com>
2022-12-01 12:36:16 +0900

    testfrom dev40

--- src/resources/views/admin/jstest1.blade.php
+++ src/resources/views/admin/jstest1.blade.php
@@ -4,32 +4,33 @@
         <input type="submit" value="検索">
     </form>
     <!--ここに表示する-->
+    <p id="number"></p>
     <p id="output"></p>
+
 </section>
 <script>
+    //神山美術館の入場料金は、一人600円であるが、5人以上のグループなら一人550円、20人以上の団体なら一人500円である。
+    // 人数を入力し、入場料の合計を計算するプログラムを作成せよ。
     //formを取得したら発動する
     document.getElementById('form').onsubmit = function(){
         var val = document.getElementById('form').word.value;
         var addpro = function(num){
-            
-        }
-    }
-/*
-    document.getElementById('form8').onsubmit = function(){
-        var val = document.getElementById('form8').word8.value;
-        var val1 = document.getElementById('form8').word9.value;
-        var addpro2 = function (num,num1){
-            var numString = String(num);
-            var numString1= String(num1);
-            var numString2;
-            for(numString= 0; numString<10; numString++){
-                for(numString1=0; numString1<10; numString1++){
-                    numString2 = numString * numString1;
-                    console.log(numString2);
-                    //document.getElementById('choice2').textContent = numString2;
-                }
+            if(num < 5 && num >= 5){
+                var numString = String(num);
+                var ans = numString * 600;
+            }else if(num >= 5 && num > 20){
+                var numString = String(num);
+                var ans = numString * 550;
+            }else if(num >= 20){
+                var numString = String(num);
+                var ans = numString * 500;
             }
+            document.getElementById('number').textContent = numString;
+            document.getElementById('output').textContent = ans;
         }
-        addpro2(val,val1);*/
+        addpro(val);
+    }
+
+
 </script>
 
