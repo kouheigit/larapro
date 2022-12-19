da5d1bac563121950d4ff53328f053ffe004fb28
kouheigit <ookawakouhei777@gmail.com>
2022-12-19 11:59:15 +0900

    testform dev44

--- src/app/Http/Controllers/Admin/SomeController.php
+++ src/app/Http/Controllers/Admin/SomeController.php
@@ -9,7 +9,7 @@ use App\Models\Someform;
 
 class SomeController extends Controller
 {
-    public function jstest2(){
+    public function jstest2(Request $request){
         return view('admin.jstest2');
     }
     public function jstest1()
--- src/resources/views/admin/jstest2.blade.php
+++ src/resources/views/admin/jstest2.blade.php
@@ -1,14 +1,12 @@
 <section>
-    <p>ありがとうございます。</p>
 </section>
 <script src="https://cdn.jsdelivr.net/npm/js-cookie@2/src/js.cookie.min.js"></script>
 <script>
-    document.getElementById('form').onsubmit = function(){
-        if(Cookies.get('answered') === 'yes'){
-            window.alert('回答済みです、アンケートは1回しか入力できません');
-            return false;
-        }else{
-            Cookies.set('answerd','yes', {experies: 7});
-        }
-    };
 </script>
+<body>
+<table>
+        @for($i=0;$i<15; $i++)
+        <li><input type="text" class="apple" id="apple{{$i}}" name="apple[]" value="{{old('apple[]')}}">-<input type="text" class="banana" id="banana{{$i}}"name="banana[]"value="{{old('banana[]')}}">-<input type="text" class="orange" id="orange{{$i}}" name="orange[]"value="{{old('orange[]')}}">-<input type="type" class="cherry" id="cherry{{$i}}" name="cherry[]" value="{{old('cherry[]')}}"></li>
+        @endfor
+</table>
+</body>
