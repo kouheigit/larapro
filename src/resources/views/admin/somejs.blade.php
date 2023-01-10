4139bb42909d9497f6834e2e07209e018d3d5007
kouheigit <ookawakouhei777@gmail.com>
2023-01-11 00:10:22 +0900

    someform dev13

--- src/resources/views/admin/somejs.blade.php
+++ src/resources/views/admin/somejs.blade.php
@@ -1,7 +1,15 @@
 <h1>これはSomeJSのフォームです。</h1>
 <script src="https://cdn.jsdelivr.net/npm/js-cookie@2/src/js.cookie.min.js"></script>
 <script>
-
+    $('.jsvalue').on('keyup',function(){
+        //jsvalueのidを取得する
+　       jsvalue_id = $(this).attr('id');
+        //id単体で取得する
+         id = jsvalue_id.replace(/[^0-9]/g, '');
+         //idのjsvalue1を出力する
+         jsvalue1_id = '#jsvalue1'+id;
+         alert(jsvalue_id);
+    });
 </script>
 <body>
 <h3>計算する値A+計算する値B✖️100</h3>
@@ -13,6 +21,7 @@
     @for($i=0;$i<15;$i++)
         <li>計算する値A<input type="text"class="jsvalue"id="jsvalue{{$i}}"name="jsvalue[]"value="{{old('jsvalue[]')}}">-計算する値B<input type="text"class="jsvalue1"id="jsvalue1"name="jsvalue1[]"value="{{old('jsvalue1[]')}}">　答え<input type="text"class="answer"id="answer{{$i}}"name="answer[]"value="{{old('answer[]')}}"></li>
     @endfor
+      　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　 合計<input type="text"class="sum"id="sum"name="sum"value="{{old('sum')}}">
 </table>
 </body>
 
