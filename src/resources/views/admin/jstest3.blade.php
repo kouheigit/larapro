b972b0a81da9c15704d989c7475a49612bfc54c8
kouheigit <ookawakouhei777@gmail.com>
2023-01-24 14:13:46 +0900

    js dev3

--- src/resources/views/admin/jstest3.blade.php
+++ src/resources/views/admin/jstest3.blade.php
@@ -18,7 +18,6 @@
             id = id_all.replace(/[^0-9]/g, '');
             name_id = "#name"+ id;
             console.log(name_id);
-            
         })
     });
 </script>
@@ -26,7 +25,7 @@
 <form method="GET" action="{{route('admin.jsget3')}}">
     <input tyep="text" class="test" id="test" name="test" value="{{old("test")}}">
     @for($i=0; $i < 15; $i++)
-        <li>検索<input tyep="text" class="search" id="search{{$i}}" name="search[{{$i}}]" value="{{old("search.$i")}}">名前<input tyep="text" class="name" id="name{{$i}}" name="name[{{$i}}]" value="{{old("name.$i")}}">-都道府県<input tyep="text" class="city" id="city{{$i}}" name="city[{{$i}}]" value="{{old("city.$i")}}">-住所<input tyep="text" class="streetname" id="streetname{{$i}}" name="streetname[{{$i}}]" value="{{old("streetname.$i")}}">番地<input tyep="text" class="streetaddress" id="streetaddress{{$i}}" name="streetaddress[{{$i}}]" value="{{old("streetaddress.$i")}}"></li>
+        <li>ID検索<input tyep="text" class="search" id="search{{$i}}" name="search[{{$i}}]" value="{{old("search.$i")}}">名前<input tyep="text" class="name" id="name{{$i}}" name="name[{{$i}}]" value="{{old("name.$i")}}">-都道府県<input tyep="text" class="city" id="city{{$i}}" name="city[{{$i}}]" value="{{old("city.$i")}}">-住所<input tyep="text" class="streetname" id="streetname{{$i}}" name="streetname[{{$i}}]" value="{{old("streetname.$i")}}">番地<input tyep="text" class="streetaddress" id="streetaddress{{$i}}" name="streetaddress[{{$i}}]" value="{{old("streetaddress.$i")}}"></li>
     @endfor
         <input type="submit" value="送信する">
 </form>
