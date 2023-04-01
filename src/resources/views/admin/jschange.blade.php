1156637c108450972f7adddffc96fb2b06cf90ec
kouheigit <ookawakouhei777@gmail.com>
2023-04-01 22:08:48 +0900

    jschange add8

--- src/resources/views/admin/jschange.blade.php
+++ src/resources/views/admin/jschange.blade.php
@@ -1,47 +1,51 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+<link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css">
+
 <script type="text/javascript">
-   function valuechange(){
-       var changeid_1 = $(this).data('value');
-       flag = confirm("ボタンID1の値を変更しますか?");
-       if( flag == true){
-           $.ajax({
-               type: "get",
-               url: "jschangeajax",
-               data :{
-                   changeid_1:changeid_1
-               },
-               cache: false,
-               dataType: 'json',
-               /*
-               data : {
-                   comment_id:comment_id,
-               },
-               cache: false,
-               dataType: 'json',*/
-           })
-       }
-   }
-   function valuechange1(){
-       var changeid_2 = $(this).data('value');
-       flag =("ボタンID2の値を変更しますか?");
-       if(flag == true){
-           $.ajax({
-               type: "get",
-               url: "jschangeajax",
-               data :{
-                   changeid_2:changeid_2
-               },
-               cache: false,
-               dataType: 'json',
-           })
-       }
-   }
+        function valuechange() {
+            var changeid_1 = $(this).data('value');
+            flag = confirm("ボタンID1の値を変更しますか?");
+            if (flag == true) {
+                $.ajax({
+                    type: "get",
+                    url: "jschangeajax",
+                    data: {
+                        changeid_1:changeid_1
+                    },
+                    cache: false,
+                    dataType: 'json',
+                    /*
+                    data : {
+                        comment_id:comment_id,
+                    },
+                    cache: false,
+                    dataType: 'json',*/
+                })
+            }
+        }
+        function valuechange1() {
+            var changeid_2 = $(this).data('value');
+            flag = ("ボタンID2の値を変更しますか?");
+            if (flag == true) {
+                $.ajax({
+                    type: "get",
+                    url: "jschangeajax",
+                    data: {
+                        changeid_2:changeid_2
+                    },
+                    cache: false,
+                    dataType: 'json',
+                })
+            }
+        }
 </script>
 <!--syamugame-->
 {{--まだbuttonの値、class name　idがちゃんと設定できていないのでmanagement.bladeをもう一度参照して作り直せ--}}
-<input type="button" class="id1"type="button"value="値がある"onclick="valuechange">
+<input type="button" class="id1"type="button"value="値がある" onclick="valuechange()">
 <input type="hidden" id="id1_hidden" name="id1_value" value={{$changeval[0]}}>
 
-<input type="button" class="id2"type="button"value="値がある"onclick="valuechange1">
+<input type="button" class="id2"type="button"value="値がある" onclick="valuechange1()">
 <input type="hidden" id="id2_hidden" name="id2_value" value={{$changeval[1]}}>
 
 
