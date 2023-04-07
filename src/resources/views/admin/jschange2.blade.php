f5dcfea563bb4d6be198a0f746f6507ca0b7b7a8
kouheigit <ookawakouhei777@gmail.com>
2023-04-07 18:52:51 +0900

    js change add 25

--- src/resources/views/admin/jschange2.blade.php
+++ src/resources/views/admin/jschange2.blade.php
@@ -4,40 +4,9 @@
 
 <script type="text/javascript">
 
-    function changevalue(){
-        var comment_id =  $(this).data('value');
-        flag = confirm("欠番の値を変更しますか?");
-        // 「はい」が押されたときの処理
-        if ( flag == true ){
-            $.ajax({
-                type: "get",
-                url: "ajaxpost",
-                data : {
-                    comment_id:comment_id,
-                },
-                cache: false,
-                dataType: 'json',
-            })
-
-            if($('#edit_comment_value').val() == 0){
-                $('#edit_comment_value').val(1);
-                $('#edit_comment').val("戻す");
-            }else{
-                $('#edit_comment_value').val(0);
-                $('#edit_comment').val("欠番");
-            }
-               alert("欠番の値の変更が完了しました");
-            }else{
-               alert("値の変更を取り消しました");
-           }
-    };
-
-
-
 $(function(){
     $('#id1').click(function() {
         var id1hidden = $('#id1hidden').val();
-        alert(id1hidden);
         flag = confirm("ボタンの値を変更しますか?");
         if(flag == true){
             $.ajax({
@@ -68,7 +37,7 @@ $(function(){
 <h1>ID番号2→{{$changeval[1]}}</h1>
 
 @if($changeval[0]==1)
-    <input type="button" id="id1" class="id1" value="値がある">
+    <input type="button" id="id1" class="id1" value="値がある"onclick>
     <input type="hidden" id="id1hidden" class="id1hidden" value={{$changeval[0]}}>
 @else
     <input type="button" id="id1" class="id1" value="値がない">
