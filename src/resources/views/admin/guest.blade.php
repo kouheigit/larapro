4ebef8fef3d58ded74b77fff5a10044abf71c011
kouheigit <ookawakouhei777@gmail.com>
2023-04-18 12:16:43 +0900

    jschange change add 25

--- src/resources/views/admin/guest.blade.php
+++ src/resources/views/admin/guest.blade.php
@@ -1,1 +1,13 @@
 <h1>くぅくぅクリケット</h1>
+
+{{--
+全文
+id
+name
+product_code
+product_code1
+product_code2--}}
+<li>購入者名　　　　　　購入商品　　　　　　購入商品1　　　　　　購入商品2</li>
+@for($i=0; $i<15; $i++)
+    <li><input type="text" class="name" id="name{{$i}}"name="name[{{$i}}]"value="{{old("name.$i")}}">　<input type="text" class="product_code" id="product_code{{$i}}"name="product_code[{{$i}}]"value="{{old("product_code.$i")}}">　<input type="text" class="product_code1"id="product_code1[{{$i}}]"value="{{old("product_code1.$i")}}">　<input type="text" class="product_code2"id="product_code2[{{$i}}]"value="{{old("product_code2.$i")}}"></li>
+@endfor
