9382098c38e408282026e8aff5b8661b1957f3d2
kouheigit <ookawakouhei777@gmail.com>
2023-04-18 13:09:04 +0900

    guest add1

--- src/resources/views/admin/guest.blade.php
+++ src/resources/views/admin/guest.blade.php
@@ -4,3 +4,4 @@
 @for($i=0; $i<15; $i++)
     <li><input type="text" class="name" id="name{{$i}}"name="name[{{$i}}]"value="{{old("name.$i")}}">　<input type="text" class="product_code" id="product_code{{$i}}"name="product_code[{{$i}}]"value="{{old("product_code.$i")}}">　<input type="text" class="product_code1"id="product_code1[{{$i}}]"value="{{old("product_code1.$i")}}">　<input type="text" class="product_code2"id="product_code2[{{$i}}]"value="{{old("product_code2.$i")}}"></li>
 @endfor
+    <b>合計</b><input type="text" class="sum" id="sum" name="sum" value="{{old("sum")}}">
