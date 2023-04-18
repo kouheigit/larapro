268a41c2c1c3b1b655c46e43d74f7f936fafccbe
kouheigit <ookawakouhei777@gmail.com>
2023-04-18 15:07:24 +0900

    guest add4

--- src/resources/views/admin/guest.blade.php
+++ src/resources/views/admin/guest.blade.php
@@ -6,8 +6,8 @@
 </scrip>
 
 <h1>購入履歴</h1>
-<li>購入者名　　　　　　購入商品　　　　　　購入商品1　　　　　　購入商品2</li>
+<li>ID　　　　　　　　　購入者名　　　　　　購入商品　　　　　　購入商品1　　　　 　購入商品2</li>
 @for($i=0; $i<15; $i++)
-    <li><input type="text" class="name" id="name{{$i}}"name="name[{{$i}}]"value="{{old("name.$i")}}">　<input type="text" class="product_code" id="product_code{{$i}}"name="product_code[{{$i}}]"value="{{old("product_code.$i")}}">　<input type="text" class="product_code1"id="product_code1[{{$i}}]"value="{{old("product_code1.$i")}}">　<input type="text" class="product_code2"id="product_code2[{{$i}}]"value="{{old("product_code2.$i")}}"></li>
+    <li><input type="text" class="id" id="id{{$i}}"name="id[{{$i}}]"value="{{old("id.$i")}}"> <input type="text" class="name" id="name{{$i}}"name="name[{{$i}}]"value="{{old("name.$i")}}">　<input type="text" class="product_code" id="product_code{{$i}}"name="product_code[{{$i}}]"value="{{old("product_code.$i")}}">　<input type="text" class="product_code1"id="product_code1[{{$i}}]"value="{{old("product_code1.$i")}}">　<input type="text" class="product_code2"id="product_code2[{{$i}}]"value="{{old("product_code2.$i")}}"></li>
 @endfor
     <b>合計</b><input type="text" class="sum" id="sum" name="sum" value="{{old("sum")}}">
