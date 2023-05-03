3aa5f3cfe80d799c3965f203785dbc206bf60f3a
kouheigit <ookawakouhei777@gmail.com>
2023-05-03 14:52:48 +0900

    guest sum comp4

--- src/resources/views/admin/guest.blade.php
+++ src/resources/views/admin/guest.blade.php
@@ -2,7 +2,6 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
 <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css">
 <script type="text/javascript">
-    {{--メリー喜多川--}}
     $(function(){
         function allsum(){
             //合計
