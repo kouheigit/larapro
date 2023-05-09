ae3d5f23a6df64fc6c1271f1a6a239803012fc80
kouheigit <ookawakouhei777@gmail.com>
2023-05-09 20:06:31 +0900

    bm dev3

--- src/app/Http/Controllers/Admin/BmController.php
+++ src/app/Http/Controllers/Admin/BmController.php
@@ -4,19 +4,14 @@ namespace App\Http\Controllers\Admin;
 use Validator;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\Jstest;
-use App\Models\Jstest3;
-use App\Models\guest;
-use App\Models\First;
-use App\Models\Someform;
-use App\Models\Testform;
+use App\Models\Billingmanagement_list;
+use App\Models\Billingmanagement_tax;
 use Carbon\Carbon;
 
 class BmController extends Controller
 {
     public function index(Request $request)
     {
-        //bmstart
         return view('admin.Bm');
     }
 }
