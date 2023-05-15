5b038ff17a9b25e0c4a97bf1337297c02bd54c47
kouheigit <ookawakouhei777@gmail.com>
2023-05-15 16:01:01 +0900

    bmlist add1

--- src/resources/views/admin/bm.blade.php
+++ src/resources/views/admin/bm.blade.php
@@ -5,5 +5,5 @@
 <br>
 <br>
 @for($i=0; $i<15; $i++)
-    <li><input type="text" class="item" id="item{{$i}}"name="item[{{$i}}]"value="{{old("item.$i")}}"><input type="text" class="quantity" id="quantity{{$i}}"name="quantity[{{$i}}]"value="{{old("quantity.$i")}}"><input type="text" class="unit" id="unit{{$i}}"name="unit[{{$i}}]"value="{{old("unit.$i")}}"><input type="text" class="price" id="price{{$i}}"name="price[{{$i}}]"value="{{old("price.$i")}}"><input type="text" class="taxation" id="taxation{{$i}}"name="taxation[{{$i}}]"value="{{old("taxation.$i")}}"><input type="text" class="sum" id="sum{{$i}}"name="sum[{{$i}}]"value="{{old("sum.$i")}}"><input type="checkbox" class="taxation" id="taxation[{{$i}}]" name="taxation[{{$i}}]" value={{$i}}></li>
+    <li>項目<input type="text" class="item" id="item{{$i}}"name="item[{{$i}}]"value="{{old("item.$i")}}"> 数量<input type="text" class="quantity" id="quantity{{$i}}"name="quantity[{{$i}}]"value="{{old("quantity.$i")}}"> 単位<input type="text" class="unit" id="unit{{$i}}"name="unit[{{$i}}]"value="{{old("unit.$i")}}"> 単価<input type="text" class="price" id="price{{$i}}"name="price[{{$i}}]"value="{{old("price.$i")}}"> 合計<input type="text" class="sum" id="sum{{$i}}"name="sum[{{$i}}]"value="{{old("sum.$i")}}"> 課税<input type="checkbox" class="taxation" id="taxation[{{$i}}]" name="taxation[{{$i}}]" value={{$i}}></li>
 @endfor
