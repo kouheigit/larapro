efb7980e1ef788eb268a4df173ad7b02db663300
kouheigit <ookawakouhei777@gmail.com>
2023-05-15 18:54:08 +0900

    bmlist add 4

--- src/resources/views/admin/bm.blade.php
+++ src/resources/views/admin/bm.blade.php
@@ -9,6 +9,7 @@
 @for($i=0; $i<15; $i++)
     <li>項目<input type="text" class="item" id="item{{$i}}"name="item[{{$i}}]"value="{{old("item.$i")}}"> 数量<input type="text" class="quantity" id="quantity{{$i}}"name="quantity[{{$i}}]"value="{{old("quantity.$i")}}"> 単位<input type="text" class="unit" id="unit{{$i}}"name="unit[{{$i}}]"value="{{old("unit.$i")}}"> 単価<input type="text" class="price" id="price{{$i}}"name="price[{{$i}}]"value="{{old("price.$i")}}"> 合計<input type="text" class="sum" id="sum{{$i}}"name="sum[{{$i}}]"value="{{old("sum.$i")}}"> 課税<input type="checkbox" class="taxation" id="taxation[{{$i}}]" name="taxation[{{$i}}]" value={{$i}}></li>
 @endfor
+    <input type="text" class="tax" id="tax" name="tax" value="{{old("tax")}}"><input type="text" class="value" id="value" name="value" value="value"><input type="text" class="subtotal" id="subtotal" name="subtotal" value="{{old("subtotal")}}"><input type="text" class="ctsv" id="ctsv" name="ctsv" value="{{old("ctsv")}}"><input type="text" class="sum" id="sum" name="sum" value="{{old("sum")}}"><input type="text" class="remarks" id="remarks" name="remarks" value="{{old("remarks")}}">
     <input type="submit"value="送信する">
 </form>
 
