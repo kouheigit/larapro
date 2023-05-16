8a92b294cd5d3d486cb2fb89ce125a1c945d74f1
kouheigit <ookawakouhei777@gmail.com>
2023-05-16 11:44:24 +0900

    bm tax dev1

--- src/resources/views/admin/bm.blade.php
+++ src/resources/views/admin/bm.blade.php
@@ -12,6 +12,7 @@
 @endfor
     <br>
     <br>
+    <!--消費税関連-->
     <b>消費税関連</b>
     <br>
         消費税<input type="text" class="tax" id="tax" name="tax" value="{{old("tax")}}">有効値<input type="text" class="value" id="value" name="value" value="{{old("value")}}">
