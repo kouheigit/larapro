571ef3889245db487adf98b8ed55637b72237e45
kouheigit <ookawakouhei777@gmail.com>
2023-05-17 17:49:01 +0900

    bm tax dev18

--- src/app/Http/Controllers/Admin/BmController.php
+++ src/app/Http/Controllers/Admin/BmController.php
@@ -18,7 +18,6 @@ class BmController extends Controller
     public function addvalue(Request $request)
     {
 
-
         $rules = [];
         //item
 
@@ -54,6 +53,7 @@ class BmController extends Controller
         $rules['quantity.12'] = 'nullable|numeric|required_with:item.12';
         $rules['quantity.13'] = 'nullable|numeric|required_with:item.13';
 
+
         $rules['unit.0'] = 'nullable|required_with:quantity.0';
         $rules['unit.1'] = 'nullable|required_with:quantity.1';
         $rules['unit.2'] = 'nullable|required_with:quantity.2';
@@ -67,6 +67,21 @@ class BmController extends Controller
         $rules['unit.11'] = 'nullable|required_with:quantity.11';
         $rules['unit.12'] = 'nullable|required_with:quantity.12';
         $rules['unit.13'] = 'nullable|required_with:quantity.13';
+
+        $rules['price.0'] = 'nullable|numeric|required_with:unit.0';
+        $rules['price.1'] = 'nullable|numeric|required_with:unit.1';
+        $rules['price.2'] = 'nullable|numeric|required_with:unit.2';
+        $rules['price.3'] = 'nullable|numeric|required_with:unit.3';
+        $rules['price.4'] = 'nullable|numeric|required_with:unit.4';
+        $rules['price.5'] = 'nullable|numeric|required_with:unit.5';
+        $rules['price.6'] = 'nullable|numeric|required_with:unit.6';
+        $rules['price.7'] = 'nullable|numeric|required_with:unit.7';
+        $rules['price.8'] = 'nullable|numeric|required_with:unit.8';
+        $rules['price.9'] = 'nullable|numeric|required_with:unit.9';
+        $rules['price.10'] = 'nullable|numeric|required_with:unit.10';
+        $rules['price.11'] = 'nullable|numeric|required_with:unit.11';
+        $rules['price.12'] = 'nullable|numeric|required_with:unit.12';
+        $rules['price.13'] = 'nullable|numeric|required_with:unit.13';
 
 
 
@@ -122,6 +137,51 @@ class BmController extends Controller
             'quantity.12.numeric'=>'数量12は数字で入力をしてください',
             'quantity.13.numeric'=>'数量13は数字で入力をしてください',
 
+
+            'unit.0.required_with'=>'単位1が入力された場合は数量1の入力は必須です',
+            'unit.1.required_with'=>'単位2が入力された場合は数量2の入力は必須です',
+            'unit.2.required_with'=>'単位3が入力された場合は数量3の入力は必須です',
+            'unit.3.required_with'=>'単位4が入力された場合は数量4の入力は必須です',
+            'unit.4.required_with'=>'単位5が入力された場合は数量4の入力は必須です',
+            'unit.5.required_with'=>'単位6が入力された場合は数量5の入力は必須です',
+            'unit.6.required_with'=>'単位7が入力された場合は数量6の入力は必須です',
+            'unit.7.required_with'=>'単位8が入力された場合は数量7の入力は必須です',
+            'unit.8.required_with'=>'単位9が入力された場合は数量8の入力は必須です',
+            'unit.9.required_with'=>'単位10が入力された場合は数量9の入力は必須です',
+            'unit.10.required_with'=>'単位11が入力された場合は数量10の入力は必須です',
+            'unit.11.required_with'=>'単位12が入力された場合は数量11の入力は必須です',
+            'unit.12.required_with'=>'単位13が入力された場合は数量12の入力は必須です',
+            'unit.13.required_with'=>'単位14が入力された場合は数量13の入力は必須です',
+
+            'price.0.required_with'=>'単位1が入力された場合は単価1の入力は必須です',
+            'price.1.required_with'=>'単位2が入力された場合は単価2の入力は必須です',
+            'price.2.required_with'=>'単位3が入力された場合は単価3の入力は必須です',
+            'price.3.required_with'=>'単位4が入力された場合は単価4の入力は必須です',
+            'price.4.required_with'=>'単位5が入力された場合は単価5の入力は必須です',
+            'price.5.required_with'=>'単位6が入力された場合は単価6の入力は必須です',
+            'price.6.required_with'=>'単位7が入力された場合は単価7の入力は必須です',
+            'price.7.required_with'=>'単位8が入力された場合は単価8の入力は必須です',
+            'price.8.required_with'=>'単位9が入力された場合は単価9の入力は必須です',
+            'price.9.required_with'=>'単位10が入力された場合は単価10の入力は必須です',
+            'price.10.required_with'=>'単位11が入力された場合は単価11の入力は必須です',
+            'price.11.required_with'=>'単位12が入力された場合は単価12の入力は必須です',
+            'price.12.required_with'=>'単位13が入力された場合は単価13の入力は必須です',
+            'price.13.required_with'=>'単位14が入力された場合は単価14の入力は必須です',
+
+            'price.0.numeric'=>'単位1は数字で入力してください',
+            'price.1.numeric'=>'単位2は数字で入力してください',
+            'price.2.numeric'=>'単位3は数字で入力してください',
+            'price.3.numeric'=>'単位4は数字で入力してください',
+            'price.4.numeric'=>'単位5は数字で入力してください',
+            'price.5.numeric'=>'単位6は数字で入力してください',
+            'price.6.numeric'=>'単位7は数字で入力してください',
+            'price.7.numeric'=>'単位8は数字で入力してください',
+            'price.8.numeric'=>'単位9は数字で入力してください',
+            'price.9.numeric'=>'単位10は数字で入力してください',
+            'price.10.numeric'=>'単位11は数字で入力してください',
+            'price.11.numeric'=>'単位12は数字で入力してください',
+            'price.12.numeric'=>'単位13は数字で入力してください',
+            'price.13.numeric'=>'単位14は数字で入力してください',
 
         ];
 
