e7f6768a24c8d26e4b3441be6361a0cd89ea2516
kouheigit <ookawakouhei777@gmail.com>
2023-06-02 21:43:04 +0900

    hasmany dev2

--- src/app/Models/Writer.php
+++ src/app/Models/Writer.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Writer extends Model
 {
     use HasFactory;
+    protected $table = 'writers';
+    protected $fillable = [
+        'name',
+    ];
 }
