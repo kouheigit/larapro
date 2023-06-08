26670138978c5623b49a4ff4f5bd2172727e2466
kouheigit <ookawakouhei777@gmail.com>
2023-06-08 11:32:08 +0900

    hasmany dev13

--- src/app/Models/Book.php
+++ src/app/Models/Book.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model
+//larapro start
 {
     use HasFactory;
     protected $table = 'books';
