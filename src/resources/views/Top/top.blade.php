95f76b87f2d866677ff02ee3ae9f862253348701
kouheigit <ookawakouhei777@gmail.com>
2023-08-21 17:58:50 +0900

    Top dev6

--- src/resources/views/Top/top.blade.php
+++ src/resources/views/Top/top.blade.php
@@ -9,11 +9,33 @@
 <br>
 {{--これで横一列でこれを5つ--}}
 {{--5泊,3泊,8泊,0泊,4泊--}}
-{{--列--}}
 <b>FREEDAY</b>
-<p>フリーデイ</p>
+<h3>フリーデイ</h3>
+{{--列--}}
+<p>利用可能日数</p>
+<p>(2022年6月末日まで有効)</p>
 <button onclick="location.href='https://qiita.com/mzmz__02'">5泊</button>
+{{--列--}}
+<p>利用可能日数</p>
+<p>(2022年12月末日まで有効)</p>
+<button onclick="location.href='https://qiita.com/mzmz__02'">3泊</button>
+{{--列--}}
+<p>利用可能日数</p>
+<p>(2023年8月末日まで有効)</p>
+<button onclick="location.href='https://qiita.com/mzmz__02'">-8泊</button>
+{{--列--}}
+<p>利用可能日数</p>
+<p>(2024年6月末日まで有効)</p>
+<button onclick="location.href='https://qiita.com/mzmz__02'">0泊</button>
+{{--列--}}
+<p>利用可能日数</p>
+<p>(2024年12月末日まで有効)</p>
+<button onclick="location.href='https://qiita.com/mzmz__02'">4泊</button>
 {{--列終了--}}
+
+
+<b>FIXDAY</b>
+<h3>フィックスデイ</h3>
 
 
 
