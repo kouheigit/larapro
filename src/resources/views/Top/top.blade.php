7a3a0498747798e123429418d036b2732404bdf9
kouheigit <ookawakouhei777@gmail.com>
2023-08-21 17:47:03 +0900

    Top dev5

--- src/resources/views/Top/top.blade.php
+++ src/resources/views/Top/top.blade.php
@@ -1,10 +1,19 @@
 <b>宿泊予約サイト</b>
+{{--↓ログイン情報をここに持ってくる--}}
 <h1>ログイン名</h1>様
-<b>お知らせ【Information】</b>
-<br>
+<b>【Information】お知らせ</b>
 <br>
 <button onclick="location.href='https://qiita.com/mzmz__02'">Information</button>
 {{--情報一覧に飛ぶ--}}
 <br>
 <br>
-<b>ナメックムーブ</b>
+{{--これで横一列でこれを5つ--}}
+{{--5泊,3泊,8泊,0泊,4泊--}}
+{{--列--}}
+<b>FREEDAY</b>
+<p>フリーデイ</p>
+<button onclick="location.href='https://qiita.com/mzmz__02'">5泊</button>
+{{--列終了--}}
+
+
+
