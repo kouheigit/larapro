308eeb797b68d7427d287bb0e13c8e7ecdb74c66
kouheigit <ookawakouhei777@gmail.com>
2023-09-04 16:46:52 +0900

    tourist dev1

---
+++ src/app/Models/Tourist.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Tourist extends Model
+{
+    use HasFactory;
+    protected $table ='tourists';
+    protected $fillable = [
+        'name',
+        'product_code',
+        'first_product',
+        'second_product',
+    ];
+}
