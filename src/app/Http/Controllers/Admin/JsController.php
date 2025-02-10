0fc371c47c44c4291ccb223de4233704b3b94f1c
kouheigit <ookawakouhei777@gmail.com>
2025-02-10 14:59:35 +0900

    test2

--- src/app/Http/Controllers/Admin/JsController.php
+++ src/app/Http/Controllers/Admin/JsController.php
@@ -44,7 +44,6 @@ class JsController extends Controller
                 $value ="ID1の値を1から0に変更しました。";
                 return response()->json($value);
             }
-            //This is git test!!
     }
     public function ajaxchange5(Request $request)
     {
